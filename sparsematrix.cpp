3d1e1ee0bfafb90acd29b35a5adef1c0fed3651f
QSXW <47172719+QSXW@users.noreply.github.com>
2019-11-08 13:18:24 +0800

    Add files via upload

--- sparsematrix.cpp
+++ sparsematrix.cpp
@@ -213,14 +213,57 @@ public:
 		return *a;
 	}
 
-	TupleSparseMatrix<ElemType> & operator* (int i)
+	TupleSparseMatrix<ElemType> & operator* (TupleSparseMatrix<ElemType> &b)
 	{
-		TupleSparseMatrix<ElemType> *a = new TupleSparseMatrix();
+		/* The time complexity using standard methods to make Matrix Multiplication 
+		   is less than using the Three Tuple Sparse Matrix. */
+		std::vector<std::vector<ElemType>> ret;
+		std::vector<std::vector<ElemType>> lmat = this->convert();
+		std::vector<std::vector<ElemType>> rmat = b.convert();
+		ElemType tmp = 0;
 
-		/* Pending complete. */
+		if (this->columns == b.rows)
+		{
+			for (int i = 0; i < this->rows; i++)
+			{
+				ret.push_back(std::vector<ElemType>(b.columns));
+				for (int j = 0; j < b.columns; j++)
+				{
+					for (int k = 0; k < this->columns; k++)
+					{
+						tmp += lmat[i][k] * rmat[k][j];
+					}
+					ret[i][j] = tmp;
+					tmp = 0;
+				}
+			}
+		}
 
-		a->nonZeroCnt = (int)a->data.size();
-		return *a;
+		return *(new TupleSparseMatrix<ElemType>(ret));
+	}
+
+	std::vector<std::vector<ElemType>> & convert()
+	{
+		/* Generally speaking, the vector would initialize the element to be zero.
+	   	*  But for safer, below is intended to automatically set all elements to 
+		*  be zero. */
+		std::vector<std::vector<ElemType>> *mat = new std::vector<std::vector<ElemType>>();
+		for (int i = 0; i < this->rows; i++)
+		{
+			mat->push_back(std::vector<ElemType>(this->columns));
+			for (int j = 0; j < this->columns; j++)
+			{
+				(*mat)[i][j] = (ElemType)0;
+			}
+		}
+		int k = 0;
+		while (k < this->nonZeroCnt)
+		{
+			(*mat)[this->data[k].row][this->data[k].column] = this->data[k].e;
+			k++;
+		}
+
+		return *mat;
 	}
 
 
@@ -239,26 +282,7 @@ std::ostream & operator<< (std::ostream &os, TupleSparseMatrix<T> &t)
         return os;
     }
 
-	/* Generally speaking, the vector would initialize the element to be zero.
-	   But for safer, below is intended to automatically set all elements to be zero.
-	*/
-    std::vector<std::vector<T>> mat;
-    for (int i = 0; i < t.rows; i++)
-    {
-		mat.push_back(std::vector<T>(t.columns));
-        for (int j = 0; j < t.columns; j++)
-        {
-            mat[i][j] = (T)0;
-        }
-    }
-
-    int k = 0;
-    while (k < t.nonZeroCnt)
-    {
-        mat[t.data[k].row][t.data[k].column] = t.data[k].e;
-		k++;
-    }
-    
+    std::vector<std::vector<T>> mat = t.convert();
     for (auto c : mat)
     {
         for (auto r : c)
@@ -271,19 +295,19 @@ std::ostream & operator<< (std::ostream &os, TupleSparseMatrix<T> &t)
     return os;
 }
 
-#if 0
+#if 1
 int main()
 {  
 	using namespace ::std;
-/*
+
     TupleSparseMatrix<int> a({
-		{ 0, 0, 1, 0, 0, 0, 0 },
-		{ 0, 2, 0, 0, 0, 0, 0 },
-		{ 3, 0, 4, 0, 0, 0, 0 },
-		{ 0, 0, 0, 5, 0, 0, 0 },
-		{ 0, 0, 0, 0, 6, 0, 0 },
-		{ 0, 2, 0, 0, 0, 7, 4 },
-		{ 3, 0, 4, 0, 0, 0, 0 }
+		{ 0, 0, 1, 0, 0, 0, 0, 0 },
+		{ 0, 2, 0, 0, 0, 0, 0, 0 },
+		{ 3, 0, 4, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 5, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 6, 0, 0, 0 },
+		{ 0, 2, 0, 0, 0, 7, 4, 0 },
+		{ 3, 0, 4, 0, 0, 0, 0, 0 }
 	});
 
 	TupleSparseMatrix<int> b({
@@ -295,27 +319,26 @@ int main()
 		{ 0, 0, 4, 0, 0, 7, 4, 0 },
 		{ 0, 0, 4, 0, 0, 7, 4, 0 }
 	});
-*/
 
-	TupleSparseMatrix<int> a({
+	TupleSparseMatrix<int> e({
 		{ 5, 2, 4 },
 		{ 3, 8, 2 },
 		{ 6, 0, 4 },
 		{ 0, 1, 6 }
 	});
 
-	TupleSparseMatrix<int> b({
+	TupleSparseMatrix<int> f({
 		{ 2, 4 },
 		{ 1, 3 },
 		{ 3, 2 }
 	});
 
 	TupleSparseMatrix<int> c = a + b;
-	cout << a;
-	cout << endl;
-	cout << b;
-	cout << endl;
-	cout << c;
+
+	TupleSparseMatrix<int> g = e * f;
+	cout << e << endl;
+	cout << f << endl;
+	cout << g << endl;
 
     std::cin.get();
     return 0;
