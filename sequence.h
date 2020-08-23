4b0368cecdfdd645d6a7d7cd00b62f9d23bf72d0
QSXW <47172719+QSXW@users.noreply.github.com>
2020-08-24 03:27:59 +0800

    Add files via upload

--- sequence.h
+++ sequence.h
@@ -4,6 +4,8 @@
 #define __SEQUENCE_H__
 #include <iostream>
 
+#define _LETTER_CASE_CONVERT_FAST_
+
 constexpr unsigned int PREVIOUS_OPERATOR_ADD = 0x10U;
 
 template<class _Mystr_>
@@ -24,6 +26,8 @@ public:
     ~CharSequence();
 
     size_t length() const;
+    CharSequence<_Mystr_>& toLower();
+    CharSequence<_Mystr_>& toUpper();
     void destroy();
     const _Mystr_ *c_str() noexcept;
 
@@ -127,6 +131,98 @@ int cscomp(const _Mystr_ *_s, const _Mystr_ *_t)
     return (*p0 - *p1);
 }
 
+char *cslower(char *const _s)
+{
+#ifdef _LETTER_CASE_CONVERT_FAST_
+    const unsigned char map[] = {
+         0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,  15,
+         16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+         32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
+         48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+         64,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+        112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122,  91,  92,  93,  94,  95,
+         96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+        112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
+        128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143,
+        144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
+        160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175,
+        176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
+        192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207,
+        208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
+        224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,
+        240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255
+    };
+
+    register unsigned char *p = (unsigned char *)_s;
+    while (*p = map[*p]) { p++; }
+#else
+    char *p = _s;
+
+    while (*p)
+    {
+        if (*p > 65 && *p < 90)
+        {
+            *p += 32;
+        }
+        p++;
+    }
+#endif
+    return _s;
+}
+
+char *csupper(char *const _s)
+{
+#ifdef _LETTER_CASE_CONVERT_FAST_
+    const unsigned char map[] = {
+          0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
+         16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+         32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
+         48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+         64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+         80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
+         96,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+         80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90, 123, 124, 125, 126, 127,
+        128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143,
+        144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
+        160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175,
+        176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
+        192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207,
+        208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
+        224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,
+        240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255
+    };
+    register unsigned char *p = (unsigned char *)_s;
+    while (*p = map[*p]) { p++; }
+#else
+    char *p = _s;
+    while (*p)
+    {
+        if (*p > 97 && *p < 122)
+        {
+            *p -= 32;
+        }
+        p++;
+    }
+#endif
+    return _s;
+}
+
+template <class _Mystr_>
+inline CharSequence<_Mystr_>& CharSequence<_Mystr_>::toLower()
+{
+    if (this->next && this->normalize());
+    cslower(this->data);
+    return *this;
+}
+
+template <class _Mystr_>
+inline CharSequence<_Mystr_>& CharSequence<_Mystr_>::toUpper()
+{
+    if (this->next && this->normalize());
+    csupper(this->data);
+    return *this;
+}
+
 template <class _Mystr_>
 CharSequence<_Mystr_>::CharSequence() : data(NULL), memory_block(data), next(NULL), size(0), threshhold(0), preop(0) { }
 
@@ -199,13 +295,13 @@ template <class _Mystr_>
 void CharSequence<_Mystr_>::destroy()
 {
     CharSequence<_Mystr_> *cs = this;
-    CharSequence<_Mystr_> *__cs = NULL;
+    CharSequence<_Mystr_> *_cs_ = NULL;
     while (cs != NULL)
     {
         free(cs->data);
-        __cs = cs->next;
+        _cs_ = cs->next;
         free(cs);
-        cs = __cs;
+        cs = _cs_;
     }
 }
 
@@ -215,18 +311,20 @@ CharSequence<_Mystr_>& CharSequence<_Mystr_>::operator =(CharSequence<_Mystr_> &
     CharSequence *cs = NULL;
     if (&_cs != this)
     {
-        cs = (_cs.preop & PREVIOUS_OPERATOR_ADD) ? &_cs : new CharSequence<_Mystr_>(_cs);
-        if (this->next) { this->next->destroy(); }
-        if (this->data) { free(this->data); }
-        this->data           = cs->data;
-        this->size          = cs->size;
-        this->threshhold    = cs->threshhold;
-        this->next          = cs->next;
-        this->preop         = 0;
-        
-        cs->data     = NULL;
-        cs->next    = NULL;
-        delete cs;
+        if (cs = (_cs.preop & PREVIOUS_OPERATOR_ADD) ? &_cs : new CharSequence<_Mystr_>(_cs))
+        {
+            if (this->next) { this->next->destroy(); }
+            if (this->data) { free(this->data); }
+            this->data           = cs->data;
+            this->size          = cs->size;
+            this->threshhold    = cs->threshhold;
+            this->next          = cs->next;
+            this->preop         = 0;
+            
+            cs->data     = NULL;
+            cs->next    = NULL;
+            delete cs;
+        }    
     }
     return *this;
 }
@@ -254,29 +352,33 @@ CharSequence<_Mystr_>& CharSequence<_Mystr_>::operator +=(const _Mystr_ *_s)
 template <class _Mystr_>
 bool CharSequence<_Mystr_>::normalize()
 {
-    CharSequence<_Mystr_> *cs =  new CharSequence<_Mystr_>(*this);
-    CharSequence<_Mystr_> *__cs = new CharSequence<_Mystr_>();
+    CharSequence<_Mystr_> *cs   = nullptr;
+    CharSequence<_Mystr_> *_cs_ = nullptr;
 
-    __cs->data           = cs->data;
-    __cs->size          = cs->size;
-    __cs->threshhold    = cs->threshhold;
-    __cs->next          = cs->next;
+    if ((cs = new CharSequence<_Mystr_>(*this)) && (_cs_ = new CharSequence<_Mystr_>()))
+    {
+        _cs_->data          = cs->data;
+        _cs_->size          = cs->size;
+        _cs_->threshhold    = cs->threshhold;
+        _cs_->next          = cs->next;
 
-    cs->data             = this->data;
-    cs->size            = this->size;
-    cs->threshhold      = this->threshhold;
-    cs->next            = this->next;
+        cs->data            = this->data;
+        cs->size            = this->size;
+        cs->threshhold      = this->threshhold;
+        cs->next            = this->next;
 
-    this->data           = __cs->data;
-    this->size          = __cs->size;
-    this->threshhold    = __cs->threshhold;
-    this->next          = __cs->next;
+        this->data          = _cs_->data;
+        this->size          = _cs_->size;
+        this->threshhold    = _cs_->threshhold;
+        this->next          = _cs_->next;
 
-    __cs->data = NULL;
-    __cs->next = NULL;
-    cs->destroy();
-    delete __cs;
-    return true;
+        _cs_->data = NULL;
+        _cs_->next = NULL;
+        cs->destroy();
+        delete _cs_;
+        return true;
+    }
+    return false;
 }
 
 template <class _Mystr_>
@@ -323,6 +425,7 @@ std::ostream& operator <<(std::ostream &_os, CharSequence<_Mystr_> *_cs)
     size_t index;
     while (_cs != NULL)
     {
+        index = 0;
         while (index < _cs->size)
         {
             putchar(_cs->data[index++]);
